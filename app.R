93df711e4a066c3f784509f7c2690f623b40851e
sdhutchins <sdhutchins@uab.edu>
2023-12-17 13:08:50 -0600

    Initial commit

---
+++ app.R
@@ -0,0 +1,174 @@
+library(shiny)
+library(rentrez)
+library(tidyverse)
+library(DT)
+library(future)
+library(promises)
+library(shinythemes)
+library(tm)
+library(SnowballC)
+library(wordcloud)
+plan(multisession) # Setup for parallel processing
+
+
+source("helpers.R")
+
+
+ui <- fluidPage(
+  theme = shinytheme("sandstone"), # Using a shinythemes theme for a modern look
+  titlePanel("PubMed Search and Abstract Viewer", windowTitle = "PubMed Data Explorer"),
+  # App Description
+  titlePanel(
+    tags$h4("This application allows users to search PubMed articles based on specific search terms and a date range. It displays a data table with article details, a line chart visualization of publications per year, and a word cloud generated from the abstracts of the articles.")
+  ),
+  br(),
+  sidebarLayout(
+    sidebarPanel(
+      textAreaInput("searchTerms", "Search Terms",
+        placeholder = "Enter terms separated by commas",
+        rows = 4
+      ),
+      numericInput("fromYear", "From Year",
+        min = 1900,
+        max = format(Sys.Date(), "%Y"),
+        value = 2015
+      ),
+      numericInput("toYear", "To Year",
+        min = 1900,
+        max = format(Sys.Date(), "%Y"),
+        value = format(Sys.Date(), "%Y")
+      ),
+      actionButton("search", "Search", class = "btn-primary"),
+      br(),
+      br(),
+      downloadButton("downloadData", "Download Data"),
+      width = 2
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Data Table", DTOutput("table")),
+        tabPanel(
+          "# of Publications by Year",
+          plotOutput("yearlyPublications")
+        ),
+        tabPanel("Wordcloud",
+                 plotOutput("wordCloud"))
+      )
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  data <- eventReactive(input$search, {
+    req(input$searchTerms) # Ensure there is input before proceeding
+
+    pubmed_ids <- fetch_pubmed_ids(input$searchTerms, input$fromYear, input$toYear)
+
+    # Initialize progress
+    total_ids <- length(pubmed_ids)
+    progress <- Progress$new(session, min = 1, max = total_ids)
+    progress$set(message = "Fetching data...", value = 0)
+    progress$set(message = paste(total_ids, "Total IDs"))
+    Sys.sleep(3)
+
+    results <- lapply(pubmed_ids, function(id) {
+      # Fetch abstract and update progress
+      result <- fetch_abstracts(id)
+      progress$inc(1)
+      progress$set(message = paste("Processing ID:", id))
+
+      # Update result with a message if abstract is not found
+      if (is.na(result$Abstract)) {
+        result$Abstract <- "No abstract found."
+      }
+
+      result
+    })
+
+    # Close the progress bar
+    progress$close()
+
+    # Combine results into one dataframe
+    do.call(rbind, results)
+  })
+
+  output$table <- renderDT({
+    datatable(data(), options = list(
+      pageLength = 30,
+      autoWidth = TRUE,
+      columnDefs = list(
+        list(
+          targets = 7, # Assuming the 'Link' column is the 5th column
+          render = JS(
+            "function(data, type, row, meta) {",
+            "  return type === 'display' && data !== null && data !== '' ?",
+            "    '<a href=\"' + data + '\" target=\"_blank\">' + data + '</a>' : data;",
+            "}"
+          )
+        )
+      )
+    ), escape = FALSE, rownames = FALSE)
+  })
+
+  # Function to download data
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("PubMed-Data-", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      data <- data() # Fetch the current data
+      write.csv(data, file, row.names = FALSE)
+    }
+  )
+
+  # Visualization: Line chart with connected lines showing the number of publications per year
+  output$yearlyPublications <- renderPlot({
+    req(data())  # Ensure that data is available
+    
+    # Count and sort the number of publications per year
+    yearly_counts <- data() %>%
+      group_by(Year) %>%
+      summarise(Count = n()) %>%
+      arrange(Year)  # Sort data by year
+    
+    # Create a ggplot line chart
+    ggplot(yearly_counts, aes(x = Year, y = Count)) +
+      theme_minimal() +
+      geom_line(aes(group=1), color = "lightblue1", linewidth = 2) +  # Connect points with lines
+      geom_point(color = "navyblue", size = 6) +  # Display points
+      labs(title = "Number of Publications per Year",
+           x = "Year",
+           y = "Number of Publications")
+  })
+  
+
+  # Word Cloud: Create a word cloud from all abstract text
+  output$wordCloud <- renderPlot({
+    req(data())
+    # abstracts <- Corpus(VectorSource(data()$Abstract))
+    # # Strip unnecessary whitespace
+    # #abstracts <- tm_map(abstracts, stripWhitespace)
+    # # Convert to lowercase
+    # abstracts <- tm_map(abstracts, tolower)
+    # # Remove conjunctions etc.
+    # abstracts <- tm_map(abstracts, removeWords, stopwords("english"))
+    # # Remove suffixes to the common 'stem'
+    # abstracts <- tm_map(abstracts, stemDocument)
+    # # Remove commas etc.
+    # abstracts <- tm_map(abstracts, removePunctuation)
+    
+    abstracts <- as.character(data()$Abstract)
+    abstracts <- paste(abstracts, sep="", collapse="") 
+    
+    wordcloud(abstracts, 
+              scale = c(3, 2),
+              min.freq=10, 
+              max.words=70, colors=brewer.pal(7,"Dark2"))
+  })
+}
+
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ helpers.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(rentrez)
+library(dplyr)
+library(DT)
+library(XML)
+
+# Function to fetch PubMed IDs based on formatted search terms
+fetch_pubmed_ids <- function(search_terms_input, from_year, to_year) {
+  # Split terms by comma and trim spaces
+  terms <- strsplit(search_terms_input, ",")[[1]]
+  terms <- sapply(terms, trimws)
+  
+  # Format terms into "(term1) OR (term2) OR ..." structure
+  formatted_terms <- paste0("(", paste(terms, collapse = ") OR ("), ")")
+  
+  # Add year range to the query
+  date_range_query <- paste0(formatted_terms, " AND (", from_year, ":", to_year, "[PDAT])")
+  
+  entrez_ids <- entrez_search(db = "pubmed", term = date_range_query, retmax=10000)
+  return(entrez_ids$ids)
+}
+
+
+# Function to fetch abstracts based on PubMed IDs
+fetch_abstracts <- function(id) {
+  article <- tryCatch({
+    entrez_fetch(db = "pubmed", id = id, rettype = "xml", parsed = TRUE)
+  }, error = function(e) {
+    cat("Error fetching article with ID:", id, "\n")
+    return(NULL)
+  })
+  
+  if (is.null(article)) {
+    cat("Article is NULL for ID:", id, "\n")
+    return(data.frame(PubMedID = id, Title = NA, Year = NA, Abstract = NA, Citations = NA, FirstAuthor = NA, Journal = NA, Link = NA, stringsAsFactors = FALSE))
+  }
+  
+  abstract_text <- xpathSApply(article, "//AbstractText", xmlValue)
+  title <- xpathSApply(article, "//ArticleTitle", xmlValue)
+  year <- xpathSApply(article, "//PubDate/Year", xmlValue)
+  citations <- xpathSApply(article, "//CitationSubset", length)  # Assuming 'CitationSubset' indicates citations
+  first_author <- xpathSApply(article, "//AuthorList/Author[1]/LastName", xmlValue)
+  journal <- xpathSApply(article, "//Journal/Title", xmlValue)
+  link <- paste0("https://pubmed.ncbi.nlm.nih.gov/", id)
+  
+  # Creating a data frame with the fetched information
+  return(data.frame(
+    PubMedID = id, 
+    Title = ifelse(length(title) > 0, title[[1]], NA),
+    Year = ifelse(length(year) > 0, year[[1]], NA),
+    Abstract = ifelse(length(abstract_text) > 0, abstract_text[[1]], NA),
+    FirstAuthor = ifelse(length(first_author) > 0, first_author[[1]], NA),
+    Journal = ifelse(length(journal) > 0, journal[[1]], NA),
+    Link = link,
+    stringsAsFactors = FALSE
+  ))
+}
